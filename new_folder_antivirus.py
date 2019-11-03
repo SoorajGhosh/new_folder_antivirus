ce4db727ecffcd2a3b300572677c913f61de7e99
SoorajGhosh <soorajghosh78@gmail.com>
2019-11-03 15:58:53 +0530

    Enter the Disk id ('C:' or 'D:' or 'E:' and press enter to see all the files in it. Walk method of os module is added and the getcwd error is fixed

--- new_folder_antivirus.py
+++ new_folder_antivirus.py
@@ -2,4 +2,10 @@ import os
 
 route = str(input('Enter the route: '))
 os.chdir(route)
-print(f'Searching for virus in {os.cwd()}')
+# current working directory
+cwd = os.getcwd()
+print(f'Searching for virus in {cwd}')
+
+
+for item in os.walk(cwd):
+	print(item)
